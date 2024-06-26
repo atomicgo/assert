d4408238b408460f992a47722afa0ad3c2d7f0aa
MarvinJWendt <git@marvinjwendt.com>
2024-06-26 13:42:02 +0200

    better docs

--- doc.go
+++ doc.go
@@ -1,7 +1,7 @@
 /*
 Package assert provides a set of assertion functions.
 Every assertion function returns a boolean. This package does not integrate into the testing package automatically.
-If you want to use this package inside unit tests, you have to check the returning boolean value and call t.Fatal() if the assertion fails.
+If you want to use this package inside unit tests, you have to check the returning boolean value and call `t.Fatal()` if the assertion fails.
 
 This library does not provide any error messages. That way the assertions can also be used in production code.
 
